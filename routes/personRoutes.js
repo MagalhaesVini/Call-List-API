e08d6dde5cbfbc87aac52e1766019a6cbfc183d1
Vinicius Magalhaes <vinizipi@gmail.com>
2024-01-31 01:38:36 -0300

    Mudar buscas de nome

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -52,7 +52,7 @@ router.get('/:nome', async (req, res) => {
 
     try {
 
-        const pessoa = await Person.find({ nome: nome })
+        const pessoa = await Person.find({ nome: { $regex: sequencia, $options: 'i' } })
 
         res.status(200).json(pessoa)
     } catch (error) {
