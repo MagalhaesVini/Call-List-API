50f03b9dd146d5d91ac79c71e4171063a1623184
Vinicius Magalhaes <vinizipi@gmail.com>
2024-03-14 17:56:40 -0300

    testes deploy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-const personRoutes = require('.api/personRoutes');
+const personRoutes = require('./routes/personRoutes');
 app.use('/person', personRoutes);
 
 app.set('json spaces', 2);
